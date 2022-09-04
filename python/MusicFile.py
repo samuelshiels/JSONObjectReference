3f34d0af8aebc4315d1e635dba1533e4d5952b84
Samuel <samuel.shiels@googlemail.com>
2022-09-04 17:10:31 +0100

    2022-09-04 17:10:31

---
+++ python/MusicFile.py
@@ -0,0 +1,11 @@
+from File import File as f
+
+class MusicFile(f):
+    _defaults = {
+        "tags": [],
+        "artwork": []
+    }
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.__dict__.update(self._defaults)
+        self.__dict__.update(kwargs)    
